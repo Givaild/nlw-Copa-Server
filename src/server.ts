c5312e990fb4940f0decfd043464013a3e69bc17
Givaild <gil_nm@hotmail.com>
2022-11-02 13:40:50 -0300

    feat:insert pool count

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,9 @@
 import Fastify from "fastify";
+import {PrismaClient} from '@prisma/client'
+
+const prisma = new PrismaClient({
+    log:['query']
+})
 
 async function bootstrap () {
 const fastify = Fastify({
@@ -7,8 +12,10 @@ const fastify = Fastify({
 
 //http://localhost:3333
 
-fastify.get('/pools/count',() => {
-  return {count:0}  
+fastify.get('/pools/count', async () => {
+   const count = await prisma.pool.count()
+   
+  return {count}  
 })
 
 await fastify.listen({port:3333})
