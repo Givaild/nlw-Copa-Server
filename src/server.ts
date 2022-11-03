226f3aba8c59ec29ad473644a3230edab065d65b
Givaild <gil_nm@hotmail.com>
2022-11-02 23:29:19 -0300

    fix:install cors dependencie

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,6 @@
 import Fastify from "fastify";
 import {PrismaClient} from '@prisma/client'
+import Cors from "@fastify/cors";
 
 const prisma = new PrismaClient({
     log:['query']
@@ -10,15 +11,19 @@ const fastify = Fastify({
     logger:true
 })
 
+await fastify.register(Cors,{
+    origin:true
+})
+
 //http://localhost:3333
 
 fastify.get('/pools/count', async () => {
    const count = await prisma.pool.count()
-   
+
   return {count}  
 })
 
-await fastify.listen({port:3333})
+await fastify.listen({port:3333,host: '0.0.0.0'})
 }
 
 bootstrap()
